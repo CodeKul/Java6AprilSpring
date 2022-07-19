ab8af294beee54c29e43994adaf2d4e427369ae2
vaibhav nexttech <vn202207@nexttechvision.com>
2022-07-19 13:07:17 +0530

    Get and put request

--- src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeController.java
+++ src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeController.java
@@ -1,9 +1,10 @@
 package com.codekul.java6AprilSpring.jpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -15,6 +16,30 @@ public class EmployeeController {
     public String saveEmployee(@RequestBody Employee employee){
         employeeRepo.save(employee);
         return "employee saved..";
+    }
+
+    @GetMapping("getEmployee")
+    public List<Employee> getEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    @PutMapping("updateEmployee")
+    public String updateEmployee(@RequestBody Employee employee){
+        employeeRepo.save(employee);
+        return "record updated";
+    }
+   @PutMapping("updateEmployee1")
+    public String updateEmployee1(@RequestBody Employee employee){
+      Optional<Employee> employee1=  employeeRepo.findById(employee.getId());
+      if(employee1.isPresent()){
+          employee1.get().setAddress(employee.getAddress());
+          employee1.get().setName(employee.getName());
+          employeeRepo.save(employee1.get());
+      }else {
+          System.out.println("id not found");
+      }
+
+      return "record updated ..";
     }
 
 }
