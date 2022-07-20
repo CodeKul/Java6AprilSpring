d438a6a5d3599ad9cee144c59fd7398de52e3713
vaibhav nexttech <vn202207@nexttechvision.com>
2022-07-20 13:18:31 +0530

    DeleteMapping

--- src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeController.java
+++ src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeController.java
@@ -13,33 +13,45 @@ public class EmployeeController {
     private EmployeeRepo employeeRepo;
 
     @PostMapping("employee/save")
-    public String saveEmployee(@RequestBody Employee employee){
+    public String saveEmployee(@RequestBody Employee employee) {
         employeeRepo.save(employee);
         return "employee saved..";
     }
 
     @GetMapping("getEmployee")
-    public List<Employee> getEmployee(){
+    public List<Employee> getEmployee() {
         return employeeRepo.findAll();
     }
 
     @PutMapping("updateEmployee")
-    public String updateEmployee(@RequestBody Employee employee){
+    public String updateEmployee(@RequestBody Employee employee) {
         employeeRepo.save(employee);
         return "record updated";
     }
-   @PutMapping("updateEmployee1")
-    public String updateEmployee1(@RequestBody Employee employee){
-      Optional<Employee> employee1=  employeeRepo.findById(employee.getId());
-      if(employee1.isPresent()){
-          employee1.get().setAddress(employee.getAddress());
-          employee1.get().setName(employee.getName());
-          employeeRepo.save(employee1.get());
-      }else {
-          System.out.println("id not found");
-      }
 
-      return "record updated ..";
+    @PutMapping("updateEmployee1")
+    public String updateEmployee1(@RequestBody Employee employee) {
+        Optional<Employee> employee1 = employeeRepo.findById(employee.getId());
+        if (employee1.isPresent()) {
+            employee1.get().setAddress(employee.getAddress());
+            employee1.get().setName(employee.getName());
+            employeeRepo.save(employee1.get());
+            return "record updated ..";
+        } else {
+            return "id not found";
+        }
     }
+
+    @DeleteMapping("deleteEmployee")
+    public String deleteEmployee(@RequestBody Employee employee){
+        employeeRepo.delete(employee);
+        return "employee deleted ..";
+    }
+    @DeleteMapping("deleteEmployee1/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeRepo.deleteById(id);
+        return "employee deleted ..";
+    }
+
 
 }
