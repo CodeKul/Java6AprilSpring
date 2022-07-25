6c5150a17f72a98099a2af45da86f7e1cca3e1a2
vaibhav nexttech <vn202207@nexttechvision.com>
2022-07-25 13:10:36 +0530

    Jpa methods

--- src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeRepo.java
+++ src/main/java/com/codekul/java6AprilSpring/jpa/EmployeeRepo.java
@@ -8,11 +8,8 @@ import java.util.List;
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 
     Employee findByName(String name);
-
     Employee findByNameAndAddress(String name, String address);
-
     List<Employee> findByDobBetween(LocalDate firstDate, LocalDate secondDate);
-
     List<Employee> findByAgeLessThan(Integer age);
     List<Employee> findByAgeGreaterThanEqual(Integer age);
     List<Employee> findByDobAfter(LocalDate dob);
