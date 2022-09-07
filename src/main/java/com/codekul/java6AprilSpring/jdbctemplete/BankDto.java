99cc13d9b65da4cc1e91a73f232988ed751c769b
vaibhav nexttech <vn202207@nexttechvision.com>
2022-09-07 12:54:36 +0530

    JdbcTemplete

---
+++ src/main/java/com/codekul/java6AprilSpring/jdbctemplete/BankDto.java
@@ -0,0 +1,16 @@
+package com.codekul.java6AprilSpring.jdbctemplete;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BankDto {
+
+    Long id;
+
+    Long accountNumber;
+
+    String ifscCode;
+
+}
